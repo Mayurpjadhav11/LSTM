d1bb7a23492ac440f36a490d37509c3f03721dbf
Mayurpjadhav11 <168913746+Mayurpjadhav11@users.noreply.github.com>
2025-04-22 17:23:24 +0200

    Preprocessing is done

--- preprocessing.py
+++ preprocessing.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import MinMaxScaler
 import os
 
 # Load CS
-df = pd.read_csv("D:/SEM_3_STUDY/LSTM_PROJECT/Data/ fabricated.csv", parse_dates=["DateTime"])
+df = pd.read_csv("D:\SEM_3_STUDY\Projects\LSTM_PROJECT\Data\ fabricated.csv", parse_dates=["DateTime"])
 
 # Check missing
 df = df.fillna(method='ffill')
@@ -27,10 +27,32 @@ df_scaled["DateTime"] = df["DateTime"]
 df_scaled["Date"] = df["Date"]
 df_scaled["Time"] = df["Time"]
 
+
+
+df_scaled["Hour_Label"] = ["T" + str((i % 24) + 1) for i in range(len(df_scaled))]
+
+
+unique_dates = df_scaled["Date"].unique()
+date_mapping = {date: f"D{i+1}" for i, date in enumerate(unique_dates)}
+
+
+df_scaled["Day_Label"] = df_scaled["Date"].map(date_mapping)
+
+
+#rearder
+cols = df_scaled.columns.tolist()
+cols_reordered = ['Day_Label', 'Hour_Label'] + [i for i in cols if i not in ['Day_Label', 'Hour_Label']]
+df_scaled = df_scaled[cols_reordered]
+df_scaled.set_index("Day_Label", inplace=False)
+
+#drip
+for col in ["DateTime", "Date", "Time"]:
+    if col in df_scaled.columns:
+        df_scaled.drop(col, axis=1, inplace=True)
+
+
 # Save scaled DataFrame
-df_scaled.to_csv("D:/SEM_3_STUDY/LSTM_PROJECT/Data/scaled_data.csv", index=False)
+df_scaled.to_csv("D:\SEM_3_STUDY\Projects\LSTM_PROJECT\Data\scaled_data.csv", index=False)
 
 print("✅ Preprocessed data saved successfully.")
-
-
 
